964eb2bbeee72ba6f4ab5204fc4bed3e9d94962e
silas441 <silas.crispim@escola.pr.gov.br>
2024-11-18 11:12:31 -0300

    aula 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 400);
+    let palavra = prompt("Qual palavra deseja   codificar?"); 
+
+    background("#01080e");
+    console.log(palavra);
+
+    noLoop();
+  }
